c5039204b9efa128162deee23337e79cc479065a
Edward Tanguay (windows) <edward@tanguay.info>
2023-05-30 15:09:34 +0200

    html is finished

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-import './style.css';
+import './style.scss';
+import exercises from './data/github-classroom-exercises.json';
 
 document.querySelector('#app').innerHTML = `
 <h1>Items with image icons</h1>
+<div clsas="exercises">
+		
+		${exercises.map(exercise => `
+		<div class="exercise">
+			<img src="images/exercise.png"><div class="info">${exercise.title}</div>
+		</div>
+		`).join('')}
+</div>
 `;
